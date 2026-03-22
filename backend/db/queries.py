9ba6f9ecc3a0a1a7d7ca5a511055983d288c5190
unknown <ngunjyal25@gmail.com>
2026-03-22 23:43:30 +0530

    fix

--- backend/db/queries.py
+++ backend/db/queries.py
@@ -1,10 +1,12 @@
 # db/queries.py
 from db.connection import get_conn, put_conn
 
+from psycopg2.extras import RealDictCursor
+
 def execute_query(query, params=None):
     conn = get_conn()
     try:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query, params)
             if cur.description:
                 result = cur.fetchall()
